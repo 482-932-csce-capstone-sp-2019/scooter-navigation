972a587491a41f4966e4aa9fbe6525d22e8c6c7b
ryanhennigan151 <ryan.hennigan@tamu.edu>
2019-03-27 11:23:51 -0500

    adding map from tutorial

--- app/src/main/java/com/scooter/scooter_nav/HomeTab.java
+++ app/src/main/java/com/scooter/scooter_nav/HomeTab.java
@@ -1,15 +1,64 @@
 package com.scooter.scooter_nav;
 
+import android.Manifest;
+import android.content.Context;
+
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import java.util.List;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+// classes needed to initialize map
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.location.modes.CameraMode;
+import com.mapbox.mapboxsdk.location.modes.RenderMode;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+import com.mapbox.mapboxsdk.location.LocationComponent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.widget.Toast;
 
-public class HomeTab extends Fragment {
+
+
+public class HomeTab extends Fragment implements OnMapReadyCallback, PermissionsListener {
+
+
+    // variables for displaying map
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+
+    private static final int MY_PERMISSIONS_ACCESS_LOCATION = 1;
+
+    // variables for locating user
+    private LocationComponent locationComponent;
+
+    private PermissionsManager permissionsManager;
+
+
 
     public static HomeTab newInstance() {
         return new HomeTab();
@@ -18,8 +67,81 @@ public class HomeTab extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_tab_fragment, container, false);
+
+
+        Mapbox.getInstance(getContext(), getString(R.string.access_token));
+        super.onCreateView(inflater, container, savedInstanceState);
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+        View rootView = inflater.inflate(R.layout.home_tab_fragment, container, false);
+
+        mapView = rootView.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+        return rootView;
+
+
+        //return inflater.inflate(R.layout.home_tab_fragment, container, false);
     }
 
 
+
+    // OnMapReadyCallback
+    @Override
+    @SuppressWarnings({"MissingPermission"})
+    public void onMapReady(MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(new Style.Builder().fromUrl("mapbox://styles/mapbox/cjerxnqt3cgvp2rmyuxbeqme7"),
+                new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+                        enableLocationComponent(style);
+                    }
+                });
+    }
+
+
+    @SuppressWarnings( {"MissingPermission"})
+    private void enableLocationComponent(@NonNull Style loadedMapStyle) {
+        // Check if permissions are enabled and if not request
+        if (PermissionsManager.areLocationPermissionsGranted(getContext())) {
+
+            // Get an instance of the component
+            LocationComponent locationComponent = mapboxMap.getLocationComponent();
+
+            // Activate with options
+            locationComponent.activateLocationComponent(getContext(), loadedMapStyle);
+
+            // Enable to make component visible
+            locationComponent.setLocationComponentEnabled(true);
+
+            // Set the component's camera mode
+            locationComponent.setCameraMode(CameraMode.TRACKING);
+
+            // Set the component's render mode
+            locationComponent.setRenderMode(RenderMode.COMPASS);
+        } else {
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(getActivity());
+        }
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) {
+        Toast.makeText(getContext(), R.string.user_location_permission_explanation, Toast.LENGTH_LONG).show();
+    }
+    @Override
+    public void onPermissionResult(boolean granted) {
+        if (granted) {
+            mapboxMap.getStyle(new Style.OnStyleLoaded() {
+                @Override
+                public void onStyleLoaded(@NonNull Style style) {
+                    enableLocationComponent(style);
+                }
+            });
+        } else {
+            Toast.makeText(getContext(), R.string.user_location_permission_not_granted, Toast.LENGTH_LONG).show();
+            //finish();
+        }
+    }
 }
--- app/src/main/java/com/scooter/scooter_nav/MainActivity.java
+++ app/src/main/java/com/scooter/scooter_nav/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
 
 
